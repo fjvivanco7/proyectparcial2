72040f8f69c32949d7ad7619cabb9bc46ae065e5
fjvivanco7 <105758129+fjvivanco7@users.noreply.github.com>
2022-07-28 02:32:34 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
@@ -6,16 +7,12 @@ app = Flask(__name__, template_folder='templates')
 app.secret_key = '123456789'
 
 @app.route('/')
-def index():
+def inicio():
     return render_template('login.html')
     
-@app.route('/inicio')
-def juego():
-    return render_template('index.html')
-
 @app.route('/start')
 def inicio():
-    return render_template('inicio.html')
+    return render_template('index.html')
 
 #Metodo para correr la aplicacion
 if __name__ == '__main__':
