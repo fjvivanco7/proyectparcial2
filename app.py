fd1bfbdd243cdaa9cc9fba32fbb825f5f4465b2c
fjvivanco7 <105758129+fjvivanco7@users.noreply.github.com>
2022-07-27 23:55:26 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,46 +1,21 @@
-#Importar la biblioteca de flask
-from flask import Flask, redirect, render_template, request, url_for, flash
+from flask import Flask, render_template
 
-#Objeto para inicilizar la aplicacion
-#1. nombre por defecto
-#2. ruta donde esta los templates o nombre de la carpeta
 app = Flask(__name__, template_folder='templates')
-
-usuarios =[]
 
 #Clave secreta de la aplicacion
 app.secret_key = '123456789'
 
-#Controlador de la ruta por defecto
 @app.route('/')
 def index():
-    return render_template('login.html',usuarios=usuarios)
-
-@app.route('/enviar',methods=["GET","POST"])
-def enviarDatos():
+    return render_template('login.html')
     
-    email=request.form.get("email")
-    password=request.form.get("password")
-    
+@app.route('/inicio')
+def juego():
+    return render_template('index.html')
 
-    if request.method =="GET":
-        return render_template("agregar.html")
-    else:   
-            data=[
-                    email,
-                    password
-                 ]
-            
-            usuarios.append(data)
-            return redirect("/")
-
-@app.route('/principal')
-def principal():
-    return render_template('principal.html')
-
-@app.route('/menu')
-def menu():
-    return render_template('menu.html')
+@app.route('/start')
+def inicio():
+    return render_template('inicio.html')
 
 #Metodo para correr la aplicacion
 if __name__ == '__main__':
